98873a0c98842f3a8a6259ac69bc0650bb9bc75f
martin <martin.lampacher@gmail.com>
2022-04-11 22:42:23 +0200

    a little try for windows

--- .github/setup/load_artifacts_windows.bat
+++ .github/setup/load_artifacts_windows.bat
@@ -2,8 +2,9 @@
 @REM powershell -Command "Invoke-WebRequest https://github.com/llvm/llvm-project/releases/download/llvmorg-14.0.0/LLVM-14.0.0-win64.exe -OutFile llvm.exe"
 
 choco install llvm --version 14.0.0
+dir "%SYSTEMDRIVE%\Program Files\LLVM\bin"
 
 mkdir "artifacts\clang\clang-format"
-copy "$($env:SystemDrive)\Program Files\LLVM\bin\clang-format.exe" "artifacts\clang\clang-format"
+copy "%SYSTEMDRIVE%\Program Files\LLVM\bin\clang-format.exe" "artifacts\clang\clang-format"
 
 artifacts\clang\clang-format.exe --version
