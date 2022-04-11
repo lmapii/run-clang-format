db689574c6604bf5a39cbf6edd5a8497f7aef232
martin <martin.lampacher@gmail.com>
2022-04-11 21:42:04 +0200

    testing simple job/checkout

---
+++ .github/setup/load_artifacts_ubuntu.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# input https://github.com/BurntSushi/ripgrep/blob/master/ci/ubuntu-install-packages
+# input https://docs.github.com/en/actions/using-github-hosted-runners/customizing-github-hosted-runners
+
+if ! command -V sudo; then
+  apt-get update
+  apt-get install -y --no-install-recommends sudo
+fi
+
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends \
+  wget
+
+ver="10.0.1"
+pkg="clang+llvm-$ver-x86_64-linux-gnu-ubuntu-18.04"
+
+wget -O clang-$ver.tgz "https://github.com/llvm/llvm-project/releases/download/llvmorg-$ver/$pkg.tar.xz"
+mkdir -p artifacts/clang
+tar -xvf clang-$ver.tgz $pkg/bin/clang-format
+mv $pkg/bin/clang-format artifacts/clang
+rm -rf $pkg
+rm clang-$ver.tgz
