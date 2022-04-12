468cbe7d6e707e2671c53a3a656e0d3e0aa337e4
martin <martin.lampacher@gmail.com>
2022-04-12 23:15:19 +0200

    added --check flag

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,8 +1,30 @@
-use std::{io, path, process};
+use std::{io, path, process, str::FromStr};
+
+#[derive(Clone)]
+struct Version {
+    major: u8,
+    minor: u8,
+    patch: u8,
+}
+
+impl FromStr for Version {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = regex::Regex::new(r".*version ([\d]+)\.([\d]+)\.([\d]+).*").unwrap();
+        let caps = re.captures(s).ok_or("Failed to match version")?;
+
+        Ok(Version {
+            major: caps[1].parse().map_err(|_| "Invalid major version")?,
+            minor: caps[1].parse().map_err(|_| "Invalid minor version")?,
+            patch: caps[1].parse().map_err(|_| "Invalid patch level")?,
+        })
+    }
+}
 
 pub struct Runner {
     cmd: path::PathBuf,
-    version: Option<String>,
+    version: Option<Version>,
 }
 
 impl Runner {
@@ -34,14 +56,16 @@ impl Runner {
     }
 
     pub fn get_version(&self) -> Option<String> {
-        self.version.clone()
+        self.version
+            .as_ref()
+            .map(|v| format!("{}.{}.{}", v.major, v.minor, v.patch))
     }
 
     pub fn get_path(&self) -> path::PathBuf {
         self.cmd.clone()
     }
 
-    pub fn check(&mut self) -> Result<(), io::Error> {
+    pub fn validate(&mut self) -> Result<(), io::Error> {
         let cmd = process::Command::new(self.cmd.as_path())
             .arg("--version")
             .output()?;
@@ -58,32 +82,45 @@ impl Runner {
         // clang-format version 4.0.0 (tags/checker/checker-279)
         let stdout = String::from_utf8_lossy(&cmd.stdout);
 
-        let re = regex::Regex::new(r".*version ([\d]+)\.([\d]+)\.([\d]+).*").unwrap();
-        let caps = re
-            .captures(&stdout)
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "Failed to match version"))?;
-
-        let version = format!("{}.{}.{}", &caps[1], &caps[2], &caps[3]);
-        self.version = Some(version);
+        self.version = Some(stdout.parse::<Version>().map_err(|err| {
+            io::Error::new(
+                io::ErrorKind::Other,
+                format!("Failed to parse --version output {}: {}", stdout, err),
+            )
+        })?);
         Ok(())
     }
 
-    pub fn format<P>(&self, file: P) -> Result<(), io::Error>
-    where
-        P: AsRef<path::Path>,
-    {
-        // execute clang-format to edit in place, using style file
-        let cmd = process::Command::new(self.cmd.as_path())
-            // .arg("--dry-run") TODO:
-            .arg(file.as_ref().as_os_str())
-            .arg("--Werror")
-            .arg("-fallback-style=none")
-            .arg("-style=file")
-            .arg("-i")
-            .output()?;
+    pub fn supports_check_or_err(&self) -> Result<(), io::Error> {
+        if self.version.is_none() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Unknown version, --dry-run requires \
+                clang-format version 10.0.1 or higher",
+            ));
+        }
 
-        if let Err(err) = Runner::eval_status(cmd.status) {
-            let stderr = String::from_utf8_lossy(&cmd.stderr);
+        // https://github.com/llvm/llvm-project/commit/6a1f7d6c9ff8228328d0e65b8678a9c6dff49837
+        let version = self.version.as_ref().unwrap();
+        if version.major < 9u8 {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!(
+                    "Invalid version {}, --dry-run check requires \
+                    clang-format version 10.0.1 or higher",
+                    self.get_version().unwrap()
+                ),
+            ));
+        }
+
+        Ok(())
+    }
+
+    fn run(mut cmd: process::Command) -> Result<(), io::Error> {
+        let output = cmd.output()?;
+
+        if let Err(err) = Runner::eval_status(output.status) {
+            let stderr = String::from_utf8_lossy(&output.stderr);
 
             if stderr.len() != 0 {
                 return Err(io::Error::new(
@@ -94,6 +131,38 @@ impl Runner {
             return Err(err);
         }
         Ok(())
+    }
+
+    pub fn run_format<P>(&self, file: P) -> Result<(), io::Error>
+    where
+        P: AsRef<path::Path>,
+    {
+        let mut cmd = process::Command::new(self.cmd.as_path());
+
+        // execute clang-format to edit in place, using style file
+        cmd.arg(file.as_ref().as_os_str())
+            .arg("-fallback-style=none")
+            .arg("-style=file")
+            .arg("--Werror")
+            .arg("-i");
+
+        Runner::run(cmd)
+    }
+
+    pub fn run_check<P>(&self, file: P) -> Result<(), io::Error>
+    where
+        P: AsRef<path::Path>,
+    {
+        let _ = self.supports_check_or_err()?;
+        let mut cmd = process::Command::new(self.cmd.as_path());
+
+        cmd.arg(file.as_ref().as_os_str())
+            .arg("-fallback-style=none")
+            .arg("-style=file")
+            .arg("--Werror")
+            .arg("--dry-run");
+
+        Runner::run(cmd)
     }
 }
 
